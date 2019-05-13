b2144c286cfb72a8952c92782984b95ef74460b4
krasics <krasicsara1@gmail.com>
2019-05-13 14:57:59 +0200

    no message

--- UserService/src/main/java/internship/UserService/controllers/UserController.java
+++ UserService/src/main/java/internship/UserService/controllers/UserController.java
@@ -18,6 +18,12 @@ import internship.UserService.model.User;
 import internship.UserService.model.User.Role;
 import internship.UserService.services.UserService;
 
+/**
+ * This class represents controller for user.
+ * @author s.krasic
+ *
+ */
+
 @Controller
 @RequestMapping("/user")
 public class UserController {
@@ -25,6 +31,12 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 	
+	/**
+	 * This method is method to save current user.
+	 * @param user represents user who we want to save.
+	 * @return saved user.
+	 * 
+	 */
 	
 	@RequestMapping(value = "/save", method = RequestMethod.POST)
 	public  ResponseEntity<User>  save(@RequestBody User user) {
@@ -32,10 +44,22 @@ public class UserController {
 		return new ResponseEntity<User> (user, HttpStatus.OK); 
 	}
 	
+	/**
+	 * This method is method to get all users.
+	 * @return list of all users.
+	 */
+	
 	@RequestMapping(value = "/getAll", method = RequestMethod.GET)
 	public ResponseEntity<List<User>> getAll(){
 		return new ResponseEntity<List<User>> (userService.findAll(),HttpStatus.OK);
 	}
+	
+	/**
+	 * This method is method to get role of the user.
+	 * @param id represents id of the user which role we want to get.
+	 * @return string of the role.
+	 * 
+	 */
 	
 	@RequestMapping(value = "/getRole/{id}",  method = RequestMethod.GET)
 	public  ResponseEntity<String>  getRole(@PathVariable Long id) {
