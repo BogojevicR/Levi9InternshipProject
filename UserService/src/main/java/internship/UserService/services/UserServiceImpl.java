8bec3021280b12a65f169d02cf563909b3ea1fc2
krasics <krasicsara1@gmail.com>
2019-05-15 10:49:04 +0200

    no message

--- UserService/src/main/java/internship/UserService/services/UserServiceImpl.java
+++ UserService/src/main/java/internship/UserService/services/UserServiceImpl.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import internship.UserService.DTOmodels.UserToLogDTO;
@@ -55,6 +54,7 @@ public class UserServiceImpl implements UserService{
 				cartRepository.save(s);
 				u.setShoppingCart(s);	
 				u.setPurchases( new ArrayList<Purchase>());
+				
 
 			}	
 			userRepository.save(u);
