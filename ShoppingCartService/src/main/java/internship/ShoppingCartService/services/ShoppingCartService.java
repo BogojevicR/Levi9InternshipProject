9d2d136e4b38ca9f3269166f03924850391e9739
krasics <krasicsara1@gmail.com>
2019-05-15 14:44:05 +0200

    Dodatak za neulogovane korisnike

--- ShoppingCartService/src/main/java/internship/ShoppingCartService/services/ShoppingCartService.java
+++ ShoppingCartService/src/main/java/internship/ShoppingCartService/services/ShoppingCartService.java
@@ -9,7 +9,7 @@ import internship.ShoppingCartService.models.ShoppingCart;
 public interface ShoppingCartService {
 
 	ShoppingCart getCart(Long cartId);
-	List<CartItem> getCartItems(Long cartId);
+	List<CartItem> getCartItems(Optional <Long> cartId);
 	boolean addItem(Optional<Long> cartId, int quantity, Long bookId);
 	boolean changeQuantity(int quantity, Long itemId);
 	boolean emptyCart(Long cartId);
